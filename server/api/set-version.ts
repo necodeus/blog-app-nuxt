c3dc6f091475b8a185404bdab612df96a5b9db0c
Dawid Smulewicz <dawid@necodeo.com>
2025-01-26 01:15:55 +0100

    feat: use new spotify data

--- server/api/set-version.ts
+++ server/api/set-version.ts
@@ -1,20 +1,11 @@
-import { defineEventHandler, getCookie, setCookie } from "h3";
-
-const versions = ["next", "astro"];
-
-const getNewVersion = (currentVersion: string) => {
-  const currentIndex = versions.indexOf(currentVersion);
-  const nextIndex = (currentIndex + 1) % versions.length;
-  return versions[nextIndex];
-};
+import { defineEventHandler, setCookie } from "h3";
 
 export default defineEventHandler((event) => {
-  const version = getCookie(event, "version") || "";
-  const newVersion = getNewVersion(version);
+  const newVersion = "astro";
 
   setCookie(event, "version", newVersion);
 
   return {
-    version,
+    version: newVersion,
   };
 });
